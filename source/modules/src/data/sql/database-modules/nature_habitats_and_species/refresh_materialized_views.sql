505d2281dacb9f43e1fbe8f45371792fc9a8aaed
luitvm <38038189+eltee1@users.noreply.github.com>
2025-02-05 11:16:06 +0100

    alter schema for load_table and refresh materialized views.

--- source/modules/src/data/sql/database-modules/nature_habitats_and_species/refresh_materialized_views.sql
+++ source/modules/src/data/sql/database-modules/nature_habitats_and_species/refresh_materialized_views.sql
@@ -1,5 +1,5 @@
 -- Refresh all materialized views
-BEGIN; REFRESH MATERIALIZED VIEW habitats; COMMIT;
-BEGIN; REFRESH MATERIALIZED VIEW relevant_habitats; COMMIT;
-BEGIN; REFRESH MATERIALIZED VIEW relevant_goal_habitats; COMMIT;
-BEGIN; REFRESH MATERIALIZED VIEW relevant_species; COMMIT;
+BEGIN; REFRESH MATERIALIZED VIEW nature.habitats; COMMIT;
+BEGIN; REFRESH MATERIALIZED VIEW nature.relevant_habitats; COMMIT;
+BEGIN; REFRESH MATERIALIZED VIEW nature.relevant_goal_habitats; COMMIT;
+BEGIN; REFRESH MATERIALIZED VIEW nature.relevant_species; COMMIT;

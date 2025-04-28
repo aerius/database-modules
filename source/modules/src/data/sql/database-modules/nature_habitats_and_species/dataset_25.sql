6e6d6d044ba57b57c295ed8621d7d5cb4d4a043b
luitvm <38038189+eltee1@users.noreply.github.com>
2025-04-28 16:00:16 +0200

    Revert "moved load_table for relevant_habitat_areas to monitor-repo; it causes a build failure when generating nature."
    This reverts commit 050cfdf5bb8fc8ca3e1dee1909f68e60ba48ef16.

--- source/modules/src/data/sql/database-modules/nature_habitats_and_species/dataset_25.sql
+++ source/modules/src/data/sql/database-modules/nature_habitats_and_species/dataset_25.sql
@@ -1,3 +1,6 @@
 {import_common 'database-modules/nature_habitats_and_species/supplied_25.sql'}
 
+-- Generated data
+BEGIN; SELECT system.load_table('relevant_habitat_areas', '{data_folder}/common/nature_habitats_and_species/24/nature.relevant_habitat_areas_20240625.txt', FALSE); COMMIT;
+
 {import_common 'database-modules/nature_habitats_and_species/refresh_materialized_views.sql'}

d97050eafa95bd11c773248dcfd427179b2b58dc
luitvm <38038189+eltee1@users.noreply.github.com>
2025-05-28 11:09:55 +0200

    bring back load_dataset_24. Isnt used in Monitor.

--- source/database/build-nature-and-grid/src/data/sql/load.rb
+++ source/database/build-nature-and-grid/src/data/sql/load.rb
@@ -2,7 +2,7 @@ add_build_constants
 
 cluster_tables
 
-run_sql "load_dataset_25.sql"
+run_sql "load_dataset_24.sql"
 run_sql "build_and_store.sql"
 
 synchronize_serials
--- source/database/build-nature-and-grid/src/data/sql/load_dataset_25.sql
+++
@@ -1,2 +0,0 @@
-{import_common_into_schema 'database-modules/nature_areas/dataset_25.sql', 'nature'}
-{import_common_into_schema 'database-modules/nature_habitats_and_species/supplied_25.sql', 'nature'}

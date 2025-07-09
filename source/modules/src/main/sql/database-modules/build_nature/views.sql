03b4c1eb48da2c895f1ee198bffc7ebb6dd34ec3
luitvm <38038189+eltee1@users.noreply.github.com>
2025-07-09 09:11:48 +0200

    use nature schema for all references nature-tables in INNER JOIN.

--- source/modules/src/main/sql/database-modules/build_nature/views.sql
+++ source/modules/src/main/sql/database-modules/build_nature/views.sql
@@ -101,11 +101,11 @@ SELECT * FROM
 
 				FROM nature.habitat_areas
 					INNER JOIN nature.habitat_types USING (habitat_type_id)
-					INNER JOIN habitat_type_sensitivity_view USING (habitat_type_id)
+					INNER JOIN nature.habitat_type_sensitivity_view USING (habitat_type_id)
 					INNER JOIN natura2000_directive_area_properties USING (assessment_area_id)
 					INNER JOIN nature.habitat_type_relations USING (habitat_type_id)
 					LEFT JOIN nature.habitat_properties USING (goal_habitat_type_id, assessment_area_id)
-					LEFT JOIN designated_habitats_view USING (habitat_type_id, assessment_area_id)
+					LEFT JOIN nature.designated_habitats_view USING (habitat_type_id, assessment_area_id)
 
 				WHERE
 					sensitive IS TRUE
@@ -128,9 +128,9 @@ SELECT * FROM
 				habitat_areas.geometry AS habitat_area_geometry
 
 				FROM nature.habitat_areas
-					INNER JOIN habitat_type_sensitivity_view USING (habitat_type_id)
+					INNER JOIN nature.habitat_type_sensitivity_view USING (habitat_type_id)
 					INNER JOIN natura2000_directive_area_properties USING (assessment_area_id)
-					INNER JOIN designated_species_to_habitats_view USING (assessment_area_id, habitat_type_id)
+					INNER JOIN nature.designated_species_to_habitats_view USING (assessment_area_id, habitat_type_id)
 					INNER JOIN nature.species USING (species_id)
 					INNER JOIN nature.species_properties USING (species_id, assessment_area_id)
 

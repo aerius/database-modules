97c5d9295f7aa8c0af6960a3307141f7e7f2de76
Martijn van Luit <martijn.van.luit@rivm.nl>
2026-05-19 17:27:33 +0200

    new import-fikles with nature-abroad loaded

--- source/modules/src/data/sql/database-modules/grid/dataset_26.sql
+++ source/modules/src/data/sql/database-modules/grid/dataset_26.sql
@@ -1,2 +1,2 @@
-BEGIN; SELECT system.load_table('receptors', '{data_folder}/common/grid/26/grid.receptors_20260518.txt'); COMMIT;
-BEGIN; SELECT system.load_table('hexagons', '{data_folder}/common/grid/26/grid.hexagons_20260518.txt'); COMMIT;
+BEGIN; SELECT system.load_table('receptors', '{data_folder}/common/grid/26/grid.receptors_20260519.txt'); COMMIT;
+BEGIN; SELECT system.load_table('hexagons', '{data_folder}/common/grid/26/grid.hexagons_20260519.txt'); COMMIT;

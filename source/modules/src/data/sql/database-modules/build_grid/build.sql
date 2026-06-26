e7acc7714869e0b34f1b4171852cb13953d529a2
luitvm <38038189+eltee1@users.noreply.github.com>
2026-06-26 13:39:15 +0200

    use old file-location for province_land_borders and undo setting search_path for nature-build.

--- source/modules/src/data/sql/database-modules/build_grid/build.sql
+++ source/modules/src/data/sql/database-modules/build_grid/build.sql
@@ -1,11 +1,5 @@
 SELECT system.raise_notice('Build: geometry_of_interests @ ' || timeofday());
-BEGIN; 
-	SET search_path TO 'grid', 'public'; 
-	SELECT grid.ae_build_geometry_of_interests(); 
-COMMIT;
+BEGIN; SELECT grid.ae_build_geometry_of_interests(); COMMIT;
 
 SELECT system.raise_notice('Build: hexagons and receptors @ ' || timeofday());
-BEGIN; 
-	SET search_path TO 'grid', 'public'; 
-	SELECT grid.ae_build_hexagons_and_receptors(); 
-COMMIT;
+BEGIN; SELECT grid.ae_build_hexagons_and_receptors(); COMMIT;

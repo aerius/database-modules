e885c20394c5fa9755ca0f401d875fbd99aad22e
luitvm <38038189+eltee1@users.noreply.github.com>
2026-08-03 13:37:49 +0200

    add loading receptor_to dataset for single-zoom-level/m26.

---
+++ source/modules/src/data/sql/database-modules/grid_receptors_to/single-zoom-level/dataset_26.sql
@@ -0,0 +1,4 @@
+BEGIN; SELECT system.load_table('receptors_to_assessment_areas', '{data_folder}/common/grid_receptors_to/single-zoom-level/26/grid.receptors_to_assessment_areas_20260803.txt'); COMMIT;
+BEGIN; SELECT system.load_table('receptors_to_critical_deposition_areas', '{data_folder}/common/grid_receptors_to/single-zoom-level/26/grid.receptors_to_critical_deposition_areas_20260803.txt');
+
+{import_common 'database-modules/grid_receptors_to/refresh_materialized_views.sql'}

9270585a07b89ba5f6014e73e1001b1f63fd054b
eltee1 <38038189+eltee1@users.noreply.github.com>
2026-08-04 09:04:17 +0200

    Freshly generated grid-nature files, without abroad nature data, also for calculator. (#28)
    * new generated grid-nature files, freshly made for the Monitor builld:
    - without abroad nature data (not wanted in the Monitor / GRIP module 1 UI)
    - with de depositions for the 13 missing zl1 hexagons
    * generated receptor_to files for single-zoom-level for Calculator

--- source/modules/src/data/sql/database-modules/grid/dataset_26.sql
+++ source/modules/src/data/sql/database-modules/grid/dataset_26.sql
@@ -1,2 +1,2 @@
-BEGIN; SELECT system.load_table('receptors', '{data_folder}/common/grid/26/grid.receptors_20260717.txt'); COMMIT;
-BEGIN; SELECT system.load_table('hexagons', '{data_folder}/common/grid/26/grid.hexagons_20260717.txt'); COMMIT;
+BEGIN; SELECT system.load_table('receptors', '{data_folder}/common/grid/26/grid.receptors_20260730.txt'); COMMIT;
+BEGIN; SELECT system.load_table('hexagons', '{data_folder}/common/grid/26/grid.hexagons_20260730.txt'); COMMIT;
